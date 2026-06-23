eea20e5a7b605314b0885b212326984357fde5ca
kurilka_signaly <kurilka.signaly@gmail.com>
2026-06-23 13:56:04 +0500

    Create amps.js

---
+++ amps.js
@@ -0,0 +1,36 @@
+// ===== ДАННЫЕ АМФ =====
+const ampsData = [
+    // Инструменты
+    { name: "Тапочки", image: "images/амфа-ускорение.webp", category: "Инструмент" },
+    { name: "Какофония", image: "images/амфа-проворность.webp", category: "Инструмент" },
+    { name: "Уменьшение шума", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    { name: "Зарядное устройство", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    { name: "Поломатор", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    { name: "Вторичное использование", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    { name: "Мастер-взломщик", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    { name: "Рюкзак", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    { name: "Замыкание", image: "images/амфа-интуиция.webp", category: "Инструмент" },
+    // Навыки
+    { name: "Прячься и дыши", image: "images/амфа-скрытность.webp", category: "Навык" },
+    { name: "Прячься и исцеляйся", image: "images/амфа-обострение.webp", category: "Навык" },
+    { name: "Прятки с пользой", image: "images/амфа-сила.webp", category: "Навык" },
+    { name: "Быстрый побег", image: "images/амфа-сила.webp", category: "Навык" },
+    { name: "Невидимый", image: "images/амфа-сила.webp", category: "Навык" },
+    { name: "Взломщик дверных ловушек", image: "images/амфа-сила.webp", category: "Навык" },
+    { name: "Вдребезги", image: "images/амфа-сила.webp", category: "Навык" },
+    { name: "Сильная рука", image: "images/амфа-сила.webp", category: "Навык" },
+    { name: "Манёвр уклонения", image: "images/амфа-сила.webp", category: "Навык" },
+    // Лекарства
+    { name: "Двойные дозы", image: "images/амфа-восстановление.webp", category: "Лекарство" },
+    { name: "Антитоксин", image: "images/амфа-выносливость.webp", category: "Лекарство" },
+    { name: "Задел", image: "images/амфа-иммунитет.webp", category: "Лекарство" },
+    { name: "Последний шанс", image: "images/амфа-защита.webp", category: "Лекарство" },
+    { name: "Инкогнито", image: "images/амфа-защита.webp", category: "Лекарство" },
+    { name: "Отличная работа", image: "images/амфа-защита.webp", category: "Лекарство" },
+    { name: "Усиление", image: "images/амфа-усиление.webp", category: "Лекарство" },
+    { name: "Самооживление", image: "images/амфа-самооживление.webp", category: "Лекарство" },
+    { name: "Эксперт по бинтам", image: "images/амфа-эксперт.webp", category: "Лекарство" },
+];
+
+// Категории амп
+const ampCategories = ["Инструмент", "Навык", "Лекарство"];
