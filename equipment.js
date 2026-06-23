17006017c496e09bf9d5ef9badee27afe5c8f071
kurilka_signaly <kurilka.signaly@gmail.com>
2026-06-23 14:08:03 +0500

    Create equipment.js

---
+++ equipment.js
@@ -0,0 +1,9 @@
+// ===== ДАННЫЕ СНАРЯЖЕНИЯ =====
+const equipmentData = [
+    { name: "Оглушение", image: "images/оглушение иконка.webp" },
+    { name: "Завеса", image: "images/завеса иконка.webp" },
+    { name: "Рентген", image: "images/рентген.webp" },
+    { name: "Исцеление", image: "images/исцеление иконка.webp" },
+    { name: "Глушилка", image: "images/глушилка иконка.webp" },
+    { name: "Баррикада", image: "images/баррикада иконка.webp" }
+];
