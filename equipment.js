93d05ed35f4e780fcd947202d2fe7105d4ce91dd
kurilka_signaly <kurilka.signaly@gmail.com>
2026-06-24 16:30:50 +0500

    Update equipment.js

--- equipment.js
+++ equipment.js
@@ -2,7 +2,7 @@
 const equipmentData = [
     { name: "Оглушение", image: "images/оглушение иконка.webp" },
     { name: "Завеса", image: "images/завеса иконка.webp" },
-    { name: "Рентген", image: "images/рентген.webp" },
+    { name: "Рентген", image: "images/рентген иконка.webp" },
     { name: "Исцеление", image: "images/исцеление иконка.webp" },
     { name: "Глушилка", image: "images/глушилка иконка.webp" },
     { name: "Баррикада", image: "images/баррикада иконка.webp" }
